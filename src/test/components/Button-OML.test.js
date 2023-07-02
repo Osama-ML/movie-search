6afbc139b877106181af511d70bdae2fee47d220
Osama Moussati Lamhamdi <66735418+blueC4P@users.noreply.github.com>
2023-07-02 19:53:54 +0100

    [FEAT][#11] add button basic test

---
+++ src/test/components/Button-OML.test.js
@@ -0,0 +1,12 @@
+/**
+ * @jest-environment jsdom
+ */
+import { ButtonOML } from '../../components/Button/Button-OML';
+
+test('Test button component to be defined', () => {
+  const buttonOML = new ButtonOML();
+  let data = document.createElement('div');
+  data.appendChild(buttonOML);
+  let newComponent = data.querySelector('button-oml');
+  expect(newComponent).toBeDefined();
+});
