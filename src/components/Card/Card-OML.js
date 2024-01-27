9c8ebfc941f9056af0c245b4744cd02a626f4c1b
Osama-ML <osamag4g@gmail.com>
2024-01-27 02:17:23 +0000

    [FEAT][#28] crear nuevo componente card

---
+++ src/components/Card/Card-OML.js
@@ -0,0 +1,21 @@
+import { styles } from './Card-OML.styles.js';
+
+export class CardOML extends HTMLElement {
+      constructor() {
+            super();
+            this.attachShadow({ mode: 'open' });
+      }
+      connectedCallback(){
+            this.render();
+      }
+      render(){
+            this.shadowRoot.innerHTML = 
+            /*html*/ 
+            `<li class="card" id=${this.getAttribute('id')}>
+                  <p class="card__title">${this.getAttribute('title')}</p>
+                  <img class="card__image" src=${this.getAttribute('img')}>
+            </li>`
+            this.shadowRoot.innerHTML += styles;
+      }
+}
+window.customElements.define('card-oml', CardOML);
---
+++ src/components/Card/Card-OML.styles.js
@@ -0,0 +1,21 @@
+export const styles = /* css */ `
+    <style>
+        .card {
+            display: block;
+            width: auto;
+            height: auto;
+            border: 1px solid #e2e2e2;
+            background-color: #4e4e4e;
+            color: #fff;
+            padding: 16px;
+            border-radius: 4px;
+        }
+        .card__title{
+            margin: 0 0 8px 0;
+        }
+        .card__image{
+            height: 200px;
+            border-radius: 10px;
+        }
+    </style>
+`;
