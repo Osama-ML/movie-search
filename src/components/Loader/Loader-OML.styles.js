35c39b01c7d9d362945b18c2c2327bffffe24674
Osama-ML <osamag4g@gmail.com>
2024-01-27 23:51:25 +0000

    [FIX][#37] arreglo loader invisible superpuesto

--- src/components/Loader/Loader-OML.styles.js
+++ src/components/Loader/Loader-OML.styles.js
@@ -1,16 +1,20 @@
 export const styles = /* css */ `
     <style>
         div {
-           color: white;
-           width: 100%;
-           height: 100%;
-           background-color: #1b1b1b;
-           display: flex;
-           justify-content: center;
-           align-items: center;
+            position: absolute;
+            top: 5px;
+            color: white;
+            width: 100%;
+            height: 100%;
+            background-color: #1b1b1b;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            z-index: 5;
         }
         .hidden {
             display: none;
+            z-index: -1;
         }
     </style>
 `;
