1a115783067bcedd8598d5eeb1584a2dc44ddbb4
Osama-ML <osamag4g@gmail.com>
2024-01-27 23:36:46 +0000

    [FEAT][#28] mejora eestilos componente card

--- src/components/Card/Card-OML.styles.js
+++ src/components/Card/Card-OML.styles.js
@@ -1,20 +1,26 @@
 export const styles = /* css */ `
     <style>
         .card {
-            display: block;
-            width: auto;
-            height: auto;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            width: 200px;
+            height: 300px;
             border: 1px solid #e2e2e2;
             background-color: #4e4e4e;
             color: #fff;
             padding: 16px;
             border-radius: 4px;
         }
+        .card:hover{
+            box-shadow: 0px 0px 8px 0px rgba(255,255,255,1);
+        }
         .card__title{
             margin: 0 0 8px 0;
         }
         .card__image{
-            height: 200px;
+            height: 250px;
+            width: 180px;
             border-radius: 10px;
         }
     </style>
